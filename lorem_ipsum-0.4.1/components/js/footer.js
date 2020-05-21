fd9948829e5ab3615d369dcb9de602a208e7b5c4
psthely <psthely.webdev@gmail.com>
2020-05-21 12:32:22 +0200

    lorem_ipsum-0.4.1; add function addAttr()

---
+++ lorem_ipsum-0.4.1/components/js/footer.js
@@ -0,0 +1,10 @@
+const footerData = {
+    text: "Integer id volutpat risus"
+}
+
+const footer = (
+    '<footer ' + addAttr('class', 'container column') + '>\
+    <div ' + addAttr('id', 'footerNav') + '></div>\
+        <p>' + footerData.text + '</p>\
+    </footer>'
+)
