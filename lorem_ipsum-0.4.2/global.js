53a07db71c177469b8d8221f22436df90456e637
psthely <psthely.webdev@gmail.com>
2020-05-22 16:50:37 +0200

    add global.js file

---
+++ lorem_ipsum-0.4.2/global.js
@@ -0,0 +1,28 @@
+
+'use strict'
+
+// declare the $ querySelector
+// string[]: selectors
+// return: string[]
+function $(selectors) {
+    return selectors.split(' ')
+}
+
+// add an attribute to an element
+// string: name
+// string: value
+// return: string
+function addAttr(name, value) {
+    return name + '="' + value + '"'
+}
+
+// render the component to the parents elements
+// string: component
+// string[]: parents
+function render(component, parents) {
+    let parentNode = null
+    parents.forEach(parent => {
+        parentNode = document.querySelector(parent)
+        parentNode.innerHTML += component
+    })
+}
