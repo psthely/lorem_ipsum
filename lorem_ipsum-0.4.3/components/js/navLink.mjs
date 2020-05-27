b74126663f845c123226b695df7a77f353223f6b
psthely <psthely.webdev@gmail.com>
2020-05-27 20:08:38 +0200

    add tachyons classes to navLink component

--- lorem_ipsum-0.4.3/components/js/navLink.mjs
+++ lorem_ipsum-0.4.3/components/js/navLink.mjs
@@ -16,13 +16,19 @@ const navLinksData = [
 // construct the list of links
 let linkList = new String()
 navLinksData.forEach(
-    link => linkList += '<li><a ' + addAttr('href', link.href) + '>' + link.name + '</a></li>'
+    link => linkList += (
+        '<li ' + addAttr('class', 'pa3') +
+        '><a ' + addAttr('href', link.href) +
+        ' ' + addAttr('class', 'no-underline pa2 f4 b bg-gray white') +
+        '>' + link.name + '</a></li>'
+    )
 )
+
 
 const navLink = (
     '<nav>\
-        <ul ' + addAttr('class', 'flex flex-row flex-wrap justify-center') + '>'
-        + linkList +
+        <ul ' + addAttr('class', 'flex flex-row flex-wrap justify-center list pa0') + '>'
+            + linkList +
         '</ul>\
     </nav>'
 )
